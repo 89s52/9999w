1fb5d219cc3d09f033b95cec1474c7ad8698710b
黄剑 <8888v@163.com>
2022-06-05 07:22:15 +0800

    添加数据库

--- app/router.js
+++ app/router.js
@@ -5,5 +5,16 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  
   router.get('/', controller.home.index);
+  
+  router.get('/user', controller.user.index);
+
+
+  router.get('/user/add', controller.user.addUser);
+  router.get('/user/edit', controller.user.editUser);
+  router.get('/user/remove', controller.user.removeUser);
+  
+
+  
 };
