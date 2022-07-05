41c0c2161a1b458933e2174a4f0e91fb768ff753
黄剑 <8888v@163.com>
2022-07-06 07:13:09 +0800

    修改plugin

--- config/plugin.js
+++ config/plugin.js
@@ -1,12 +1,12 @@
-'use strict';
+﻿'use strict';
 
 /** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-};
+// module.exports = {
+// had enabled by egg
+// static: {
+//   enable: true,
+// }
+// };
 
 //启用 egg-mongoose 插件
 exports.mongoose = {
